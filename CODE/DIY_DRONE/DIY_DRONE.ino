23f478d9f54e7a0993900b09de34ad0cbedf3ef7
Lukas Kasticky <lukas@kasticky.com>
2018-02-16 16:17:36 +0100

    nothimg

---
+++ CODE/DIY_DRONE/DIY_DRONE.ino
@@ -0,0 +1,17 @@
+// pin config
+int usonic_trigger = 2;
+int usonic_echo[6] = [3, 4, 5, 7, 8, 12];
+int motor[4] = [6, 9, 10, 11];
+int gyro_sda = A4;
+int gyro_scl = A5;
+
+
+void setup()
+{
+    
+}
+
+void loop()
+{
+
+}
